6b101fcf95f932bdb0828d0ffee234afeec21aff
kirito <504831261@qq.com>
2023-03-21 14:53:00 +0800

    hub run

--- service/platform/platform.go
+++ service/platform/platform.go
@@ -9,6 +9,7 @@ import (
 	"chatim/service/platform/internal/svc"
 
 	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/threading"
 	"github.com/zeromicro/go-zero/rest"
 )
 
@@ -24,6 +25,9 @@ func main() {
 	defer server.Stop()
 
 	ctx := svc.NewServiceContext(c)
+	threading.GoSafe(func() {
+		ctx.WsHub.Run()
+	})
 	handler.RegisterHandlers(server, ctx)
 
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
